61307a029b623b9b7a1374b6bdcedc7287991f90
Ava Matthew <washington_ava@yahoo.com>
2020-06-16 18:03:20 -0500

    Updates

---
+++ precache-manifest.90b0bac87affdf5f1c8349e40b67c277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b76867686127be611be9a957170cdb",
+    "url": "/KnightsOfAvalon/movie_search_app/index.html"
+  },
+  {
+    "revision": "1f4c0fb1aca66eba1641",
+    "url": "/KnightsOfAvalon/movie_search_app/static/css/main.4ce0ff93.chunk.css"
+  },
+  {
+    "revision": "44a317ae193358714cb9",
+    "url": "/KnightsOfAvalon/movie_search_app/static/js/2.5979129a.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/KnightsOfAvalon/movie_search_app/static/js/2.5979129a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4c0fb1aca66eba1641",
+    "url": "/KnightsOfAvalon/movie_search_app/static/js/main.5994946e.chunk.js"
+  },
+  {
+    "revision": "edd98015aab660fe9a61",
+    "url": "/KnightsOfAvalon/movie_search_app/static/js/runtime-main.818b85f0.js"
+  }
+]);
